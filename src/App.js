e177a9f5ccbf52f48fd96b052b3692938ca0c54f
SahhaShin <tksgk2598@naver.com>
2023-05-03 22:15:29 +0900

    메인페이지 상품업로드 경로 구축 antd 적용_230503

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 //index.js 생략 가능 
 import MainPageCompnent from "./main/index.js"
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Link, useHistory} from 'react-router-dom';
 import UploadPage from './upload';
 import ProductPage from './product';
+import {Alert, Button} from "antd";
+import {DashOutlined, DownloadOutlined} from '@ant-design/icons';
 
 function App() {
+  const history = useHistory();
   return (
     <div>
       <div id="header">
         <div id="header-area">
-          <img src="/images/icons/logo.png" />
+          <Link to="/"><img src="/images/icons/logo.png" /></Link>
+          <Button size="large" onClick={function(){
+            history.push('/upload');
+          }} icon={<DownloadOutlined/>}>상품 업로드</Button>
         </div>
       </div>
       <div id="body">
